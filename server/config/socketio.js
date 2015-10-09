1aa7788bf3860af9c667594d1d83618fb39dc930
Nikolaos Alexiou <nikolaos.alexiou@gmail.com>
2015-10-09 16:21:03 -0400

    added chart.js on main page

--- server/config/socketio.js
+++ server/config/socketio.js
@@ -18,6 +18,7 @@ function onConnect(socket) {
   });
 
   // Insert sockets below
+  require('../api/whatsit/whatsit.socket').register(socket);
   require('../api/thing/thing.socket').register(socket);
 }
 
