513ce604efa223af3d386b22360791dc31222c76
Nikolaos Alexiou <nikolaos.alexiou@gmail.com>
2015-10-11 23:22:30 -0400

    new page files

--- client/app/newpage/newpage.controller.js
+++ client/app/newpage/newpage.controller.js
@@ -1,13 +1,6 @@
 'use strict';
 
-angular.module('basejumps2App', [])
+angular.module('basejumps2App')
   .controller('NewpageCtrl', function ($scope) {
    $score.message ="hello!"
-  // $scope.labels = ['2006', '2007', '2008', '2009', '2010', '2011', '2012'];
-  // $scope.series = ['Series A', 'Series B'];
-
-  // $scope.data = [
-  //   [65, 59, 80, 81, 56, 55, 40],
-  //   [28, 48, 40, 19, 86, 27, 90]
-  // ];
   });
